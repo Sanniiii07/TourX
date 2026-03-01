f27459a9fd14e166fa583240324c30ec4f358081
Sanniiii07 <241551073@kiit.ac.in>
2026-03-01 19:44:45 +0530

    Features in landing page added

---
+++ frontend/vite-project/src/tourist/components/Features.jsx
@@ -0,0 +1,30 @@
+function Features(){
+    return(
+        <section className="features">
+            <div className="features-content">
+                <h2>Why Choose TourX?</h2>
+
+                <div className="features-grid">
+
+                    <div className="feature-card">
+                        <h3>Secure Travel</h3>
+                        <p>Your safety is our top priority with verified guides.</p>
+                    </div>
+
+                    <div className="features-card">
+                        <h3>Affordable Packages</h3>
+                        <p>Best prices with premium experience guaranteed.</p>
+                    </div>
+
+                    <div className="features-card">
+                        <h3>24/7 Support</h3>
+                        <p>We are always here to assist you anytime.</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    );
+}
+export default Features;
