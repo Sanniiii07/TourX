35a8f9c70c4141faad593b43c093c730d8a590aa
Sanniiii07 <241551073@kiit.ac.in>
2026-03-03 21:48:49 +0530

    connections made with react router

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Landing from "./tourist/pages/Landing";
+import touristHome from "./tourist/pages/touristHome";
 import PoliceDashboard from "./pages/PoliceDashboard";
 import PoliceLogin from "./pages/PoliceLogin";
-function App() {
-  const [count, setCount] = useState(0)
-  const [collapsed, setCollapsed] = useState(false);
-  const [activeNav, setActiveNav] = useState("dashboard");
-    return (
-      <PoliceDashboard/>
-      //
-
-    )
+function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing/>} />
+      <Route path="/Login" element={<PoliceLogin/>}/>
+      <Route path="/TouristHome" element={<touristHome/>}/>
+      <Route path="/PoliceDashboard" element={<PoliceDashboard/>}/>
+      </Routes>
+      </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
